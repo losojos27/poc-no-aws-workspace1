730c6853eb46327c27aa12e06f6b1a328bce46ca
Lee Owensby <lee.owensby@gmail.com>
2024-12-18 11:33:00 -0600

    first commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
+
+variable "pet_name_length" {
+  type        = number
+  description = "Number of words in pet name"
+  default     = 5
+}
+
+variable "animal_name_length" {
+  type        = number
+  description = "Number of words in animal name"
+  default     = 5
+}
+
+module "pet" {
+  source = "./terraform-random-petname"
+
+  pet_name_length = var.pet_name_length
+}
+
+module "animal" {
+  source = "./terraform-random-petname"
+
+  pet_name_length = var.animal_name_length
+}
+
+output "pet_name" {
+  description = "Here is the name of your new pet!"
+  value       = module.pet.name
+
+}
+
+output "animal_name" {
+  description = "Here is the name of your new animal!"
+  value       = module.animal.name
+}
